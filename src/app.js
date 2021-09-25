be8ebfb4fc870d520255bd49ef9ebca67add69f3
jdru32 <87949859+jdru32@users.noreply.github.com>
2021-09-25 11:23:59 +0100

    Added weather icons
    Weather app now displays relevant weather icon and alt text

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   //The temp is inside an object called main, inside an object called data
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -36,10 +37,17 @@ function displayWeather(response) {
   //OpenWeather provides dt or date time
   //We are sending the timestamp from OpenWeather to our formatDate function which will turn dt into date and time
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  //OpenWeather provides weather condition codes that relate to specific images
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "9a2a40fbafb3cdf4386821927d8245af";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "budapest";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //To access this api, we need to tell axios to get the URL
 axios.get(apiUrl).then(displayWeather);
