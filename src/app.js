9cba482706f406364035e18b2c0ba923b2e6cb2b
jdru32 <87949859+jdru32@users.noreply.github.com>
2021-09-30 15:46:40 +0100

    Changed http to https
    In all links

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayForecast(response) {
         `<div class="col-2">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
-        src="http://openweathermap.org/img/wn/${
+        src="https://openweathermap.org/img/wn/${
           forecastDay.weather[0].icon
         }@2x.png"
         alt="sunny"
@@ -99,7 +99,7 @@ function displayWeather(response) {
   //OpenWeather provides weather condition codes that relate to specific images
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   getForecast(response.data.coord);
